ddc2015378dac161f4fa81e7707907e023bd4016
Nikolay Nikolaev <nikolaev.a.nikolay@gmail.com>
2017-07-10 19:17:33 +0300

    Add form component + Add some styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="hero is-dark">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <img src={logo} className="app-logo" alt="logo" />
+              <h2 className="title">Welcome to React</h2>
+              <h2 className="subtitle">Simple address book.</h2>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="section">
+          <div className="container">
+            <Form />
+          </div>
+        </div>
       </div>
     );
   }
